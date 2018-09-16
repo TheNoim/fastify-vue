15c8ea4aa6848b3f3ba527a9fba7e6d72e1994f7
Nils <nilsbergmann@noim.io>
2018-09-16 12:41:56 +0200

    Add typings

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+/// <reference path="node_modules/fastify/fastify.d.ts" />
+
+import * as fastify from 'fastify';
+import * as http from "http";
+import {RegisterOptions} from "fastify";
+import {Plugin} from "fastify";
+import {RequestHandler} from "fastify";
+
+declare interface HttpServer extends http.Server {}
+declare interface HttpRequest extends http.IncomingMessage {}
+declare interface HttpResponse extends http.ServerResponse {}
+
+declare interface FastifyVueOptions {
+    config?: Object
+}
+
+declare let fastifyVue: fastify.Plugin<HttpServer, HttpRequest, HttpResponse, FastifyVueOptions>;
+
+declare module 'fastify' {
+
+    export interface FastifyInstance<HttpServer = http.Server, HttpRequest = http.IncomingMessage, HttpResponse = http.ServerResponse> extends FastifyInstance {
+        register<T extends RegisterOptions<HttpServer, HttpRequest, HttpResponse>>(plugin: Plugin<HttpServer, HttpRequest, HttpResponse, T>, opts?: FastifyVueOptions | T): FastifyInstance<HttpServer, HttpRequest, HttpResponse>
+
+        // noinspection SpellCheckingInspection
+        nuxt(path: String, options?: NuxtDecoratorOptions): void
+    }
+
+    export interface NuxtDecoratorOptions {
+        handler?: RequestHandler<HttpRequest, HttpResponse>
+    }
+}
+
+export = fastifyVue;
